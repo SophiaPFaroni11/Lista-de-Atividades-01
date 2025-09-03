a0304b58d4e237f2e2949e6a2e7c471675ce66bc
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-03 10:59:46 -0300

    Feat: atividade 04

---
+++ 04-Atividade/04-Atividade/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero1;
+int numero2;
+Console.WriteLine("Digite a altura do retângulo:");
+numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a largura do retângulo:");
+numero2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"A área do retângulo é: {numero1*numero2}");
+
+
