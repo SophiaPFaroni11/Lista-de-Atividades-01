2d2765d68a469fc58183b20c05091bb40c70b71e
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-03 10:51:48 -0300

    feat: atividade 03

---
+++ 03-Atividade/03-Atividade/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero;
+Console.WriteLine("Digite um número:");
+numero = int.Parse(Console.ReadLine());
+Console.WriteLine($"O antecessor do número é: {numero-1}");
+
+
