f46f16baff69c050af72484e0461dc6e0c4c313f
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-03 11:50:08 -0300

    Feat; atividade 06

---
+++ 06-Atividade/06-Atividade/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+float salarioMensal;
+float reajuste;
+Console.WriteLine("Digite seu sálario mensal:");
+salarioMensal = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor que deseja o reajuste em porcentagem:");
+reajuste = float.Parse(Console.ReadLine());
+Console.WriteLine($"Digite seu sálario atual é {(reajuste/100*salarioMensal)+salarioMensal} com o reajuste de {reajuste / 100 * salarioMensal}");
