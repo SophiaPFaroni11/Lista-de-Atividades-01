aeeada0accf64fa44bd735abcff2cfeaee2495a6
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-03 11:26:04 -0300

    Feat: atividade 04

---
+++ 04-Atividade/04-Atividade/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int anos;
+int dias;
+Console.WriteLine("Digite quantos anos você tem:");
+anos = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite quantos dias atrás foi seu aniversário:");
+dias = int.Parse(Console.ReadLine());
+Console.WriteLine($"Você tem {anos * 365 + dias} de vida");
