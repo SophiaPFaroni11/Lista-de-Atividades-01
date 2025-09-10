aab5f295a39aa166c39b5caf5fad91e538132eb8
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 11:11:12 -0300

    Feat: EXTRA 04 finalizada

---
+++ 04-EXTRA/04-EXTRA/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+double preco;
+Console.WriteLine("Digite o preço do produto para ganhar o deconto de 5%");
+preco = double.Parse(Console.ReadLine());
+Console.WriteLine($"O preço promocional é {preco * 0.95}");
