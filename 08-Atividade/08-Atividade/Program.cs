d6c6671a4ecdeb413211fe9eab734b6c010602d1
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 08:15:11 -0300

    Feat: Atividade 08 finalizada

---
+++ 08-Atividade/08-Atividade/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+float numero;
+Console.WriteLine("Escreva um valor que direi se ele é negativo ou positivo (Sem ser 0).");
+numero = int.Parse(Console.ReadLine());
+if (numero > 0)
+{
+    Console.WriteLine("Seu número é POSITIVO.");
+}
+else
+{
+    Console.WriteLine("Seu número é NEGATIVO");
+}
