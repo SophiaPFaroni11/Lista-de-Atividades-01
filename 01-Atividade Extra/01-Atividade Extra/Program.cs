8d16227b0aa4428b2b5024e2fa3c3341b4ed1a3a
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 10:40:48 -0300

    Feat: extra 01 finalizada

---
+++ 01-Atividade Extra/01-Atividade Extra/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+double dinheiro, dolar;
+Console.WriteLine("Qual o valor presente em sua carteira?");
+dinheiro = double.Parse(Console.ReadLine());
+dolar = dinheiro / 3.45;
+Console.WriteLine($"Você tem U${dolar} ");
