4bc345f5d735a726731be085ce619a6ad622c827
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 11:17:49 -0300

    Feat: EXTRA 05 finalizada

---
+++ 05-EXTRA/05-EXTRA/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+double salario;
+Console.WriteLine("Digite seu salário atual para obter 15% de aumento.");
+salario = double.Parse(Console.ReadLine());
+Console.WriteLine($"O salário atual é {salario * 1.15}");
