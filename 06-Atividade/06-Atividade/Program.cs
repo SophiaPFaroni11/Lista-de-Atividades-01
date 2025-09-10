0d2d91eff8aa97b44dd966804c7084e30ae6db95
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 11:28:00 -0300

    Feat: atividade 06 finalizada

--- 06-Atividade/06-Atividade/Program.cs
+++ 06-Atividade/06-Atividade/Program.cs
@@ -6,3 +6,4 @@ salarioMensal = float.Parse(Console.ReadLine());
 Console.WriteLine("Digite o valor que deseja o reajuste em porcentagem:");
 reajuste = float.Parse(Console.ReadLine());
 Console.WriteLine($"Digite seu sálario atual é {(reajuste/100*salarioMensal)+salarioMensal} com o reajuste de {reajuste / 100 * salarioMensal}");
+
