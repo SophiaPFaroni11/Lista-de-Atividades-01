522723c7aeab9e1c2512df7f055a55e281526a00
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 11:04:28 -0300

    Feat: EXTRA 03 finalizada

---
+++ 03-EXTRA/03-EXTRA/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a, b, c, delta;
+Console.WriteLine("Digite  valor de A.");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite  valor de B.");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite  valor de C.");
+c = double.Parse(Console.ReadLine());
+delta = (b * b) - 4 * a * c;
+Console.WriteLine($"O valor de delta é {delta}");
+
