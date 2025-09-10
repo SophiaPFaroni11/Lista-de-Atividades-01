7f6e92f46301aef6bfbac63fc104cc907541015e
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 10:51:44 -0300

    Feat: EXTRA 02 finalizada

---
+++ 02-EXTRA/02-EXTRA/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+double altura, largura, area;
+Console.WriteLine("Digite a ALTURA a parede.");
+altura = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a LARGURA a parede.");
+largura = int.Parse(Console.ReadLine());
+area = largura * altura;
+Console.WriteLine($"Você vai precisar de {area / 2} latas de tintas");
+
