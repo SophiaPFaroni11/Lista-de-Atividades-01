054c2778be5dc188eb4d84c1f7e9c00e9849b08c
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 08:10:10 -0300

    Feat: atividade concluida

---
+++ 07-Atividade/07-Atividade/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+float numero;
+Console.WriteLine("Escreva um valor que direi se ele é ou não maior que 10.");
+numero = int.Parse(Console.ReadLine());
+if (numero > 10)
+{
+    Console.WriteLine("Seu número é maior que 10");
+}
+else
+{
+    Console.WriteLine("Seu número é menor que 10");
+}
