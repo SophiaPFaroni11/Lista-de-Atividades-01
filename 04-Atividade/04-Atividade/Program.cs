3d2960f8887a1ca4b3af12acdfb7e34800542e7e
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 11:26:06 -0300

    Feat: atividade 04 finalizada

--- 04-Atividade/04-Atividade/Program.cs
+++ 04-Atividade/04-Atividade/Program.cs
@@ -6,3 +6,4 @@ anos = int.Parse(Console.ReadLine());
 Console.WriteLine("Digite quantos dias atrás foi seu aniversário:");
 dias = int.Parse(Console.ReadLine());
 Console.WriteLine($"Você tem {anos * 365 + dias} de vida");
+
