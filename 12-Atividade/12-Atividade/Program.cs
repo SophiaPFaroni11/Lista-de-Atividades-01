5ca1488bdeafd0c52a50d66997de30367654e74f
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 09:24:23 -0300

    Feat: atividade 12 finalizada

---
+++ 12-Atividade/12-Atividade/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero1, numero2;
+Console.WriteLine("Digite o 1º número.");
+numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o 2º número.");
+numero2 = int.Parse(Console.ReadLine());
+if(numero1 > numero2)
+{
+    Console.WriteLine($"O seu maior número é {numero1}");
+}
+else
+{
+    Console.WriteLine($"O seu maior número é {numero2}");
+}
