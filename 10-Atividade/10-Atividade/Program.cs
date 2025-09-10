e2a5f7b1e189c84693ac44ba3a89917800b45b7f
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-10 08:57:41 -0300

    Feat: aividade 10 finalizada

---
+++ 10-Atividade/10-Atividade/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+double nota1, nota2, media;
+Console.WriteLine("Escreva sua 1º Nota.");
+nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Escreva sua 2º Nota.");
+nota2 = double.Parse(Console.ReadLine());
+Console.WriteLine($"Sua média foi: {media=(nota1+nota2)/2}");
+if (media >= 6)
+{
+    Console.WriteLine("APROVADO");
+}
+else
+{
+    Console.WriteLine("NÃO APROVADO");
+}
+
