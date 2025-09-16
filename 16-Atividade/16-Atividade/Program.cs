5d4f730d5a3683db0e20dc3e3feddcdb91f70c17
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-16 08:58:49 -0300

    Feat: atividade 16 finalizada

---
+++ 16-Atividade/16-Atividade/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero, i;
+Console.WriteLine("Digite um número para obter seu fatorial (!).");
+Console.WriteLine("Sabendo que fatorial negativo não existe.");
+numero = int.Parse(Console.ReadLine());
+int fatorial = 1;
+for (i = 1;  i <= numero; i++)
+{
+    fatorial *= i;
+}
+Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+
