b141efd6662bda471691ee6f74ae408f67ad719c
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-16 08:15:55 -0300

    Feat: atividade 14 finalizada

---
+++ 14-Atividade/14-Atividade/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero, resultado, quantidade;
+Console.WriteLine("Digie o último numero");
+numero = int.Parse(Console.ReadLine());
+Console.WriteLine($"Os números de 1 a {numero} são:");
+int contador = 1;
+while (contador <= numero)
+{
+    Console.WriteLine($"{contador}");
+    contador++;
+}
