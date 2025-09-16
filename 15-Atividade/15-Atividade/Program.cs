7b48e17c4dd62b324c3432b27a98fd585480c0ff
Sophia Prates <sophiaprattesfaroni@gmail.com>
2025-09-16 08:36:43 -0300

    Feat: atividade 15 finalizada

---
+++ 15-Atividade/15-Atividade/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero;
+Console.WriteLine("mostramos o número de 1.000 a 2.000");
+for (int contador = 1000; contador<= 2000; contador++)
+{
+    if (contador % 11 ==5)
+    {
+        Console.WriteLine($"{contador}");
+    }
+}
